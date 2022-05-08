7552db64e027cc467b515fa1047fcf43881f5bad
schleising <stephen@schleising.net>
2022-05-08 17:24:51 +0100

    Add data files to setup.py to ensure they're available to the application

--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ Usage:
 from setuptools import setup
 
 APP = ['main.py']
-DATA_FILES = []
+DATA_FILES: list[str] = [
+    ('ImageViewer/Resources', ['ImageViewer/Resources/285658_blue_folder_icon.png']),
+]  # type: ignore
 
 # A custom plist for file associations
 Plist = dict(
@@ -45,7 +47,7 @@ OPTIONS = {
 setup(
     name='PyImageViewer',
     app=APP,
-    data_files=DATA_FILES,
+    data_files=DATA_FILES,  # type: ignore
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
     version='0.0.0',
