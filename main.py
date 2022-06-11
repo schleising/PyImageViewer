ecc2ecd6984ece6c88f4386f2f35a85a18c1098c
schleising <stephen@schleising.net>
2022-06-11 12:38:02 +0100

    Reenable full screen

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from ImageViewer.ImageViewer import ImageViewer
 
 def main() -> None:
     # Start the image viewer
-    ImageViewer(fullScreenAllowed=False)
+    ImageViewer(fullScreenAllowed=True)
 
 if __name__ == '__main__':
     main()
